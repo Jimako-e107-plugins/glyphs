5d8ce28c70d4463d8819bb22110128dfb3960cf3
Jimako <janamicek@gmail.com>
2025-01-17 16:40:11 +0100

    Fix for correct path without renaming a reorder files [tmp]

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # glyphs
 e107 helper plugin for loading icons pack
+
+
+## Changelog
+
+1.0.1 Path's fix in e_header.php
--- glyphs/e_header.php
+++ glyphs/e_header.php
@@ -19,16 +19,48 @@ if (deftrue('USER_AREA')) // prevents inclusion of JS/CSS/meta in the admin area
 	/* supported packs only */
 	$curVal = e107::pref('glyphs', 'packs');
 	$packs = e107::unserialize($curVal);
+ 
 	$filteredPacks = array_filter($packs, function ($value)
 	{
 		return $value == 1; // Retain only keys with a value of 1
 	});
-	 
+
+	$path = e_PLUGIN  . "glyphs/glyphs.xml";
+
+	$xml = e107::getXml();
+
+	$vars = $xml->loadXMLfile($path, 'advanced');
+
+	$vars['glyphs'] = array();
+
+	if (!empty($vars['glyphicons']['glyph']))
+	{
+		foreach ($vars['glyphicons']['glyph'] as $val)
+		{
+			if (isset($val['@attributes']['name']))  $name =  $val['@attributes']['name'];
+			else continue;
+
+			$vars['glyphs'][$name] = array(
+				'name'    => $name,
+				'pattern' => isset($val['@attributes']['pattern']) ? $val['@attributes']['pattern'] : '',
+				'path'    => isset($val['@attributes']['path']) ? e107::getParser()->replaceConstants($val['@attributes']['path'], 'full') : '',
+				'class'   => isset($val['@attributes']['class']) ? $val['@attributes']['class'] : '',
+				'prefix'  => isset($val['@attributes']['prefix']) ? $val['@attributes']['prefix'] : '',
+				'tag'     => isset($val['@attributes']['tag']) ? $val['@attributes']['tag'] : '',
+			);
+		}
+
+		unset($vars['glyphicons']);
+	}
+
+ 
+	$glyphs =  $vars['glyphs'];
+
 	//shortcut, css file name with the same name
 	foreach ($filteredPacks as $pack => $value)
-	{
-
-		e107::css("glyphs", "{$pack}/css/{$pack}.min.css");
+	{ 
+		$file = $glyphs[$pack]['path'];
+		e107::css("glyphs", $file);
 	}
 
 }
